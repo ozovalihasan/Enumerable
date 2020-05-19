ac9a77e41ef35fe85e4fe143bc7c0220504fefa5
Hasan Ozovali <ozovalihasan@gmail.com>
2020-05-19 12:10:39 +0300

    Fix errors found by Rubocop

--- main.rb
+++ main.rb
@@ -7,7 +7,7 @@ module Enumerable
   end
 
   def my_each_with_index
-    size.times { |counter| yield(self[counter], counter) } if block_given? 
+    size.times { |counter| yield(self[counter], counter) } if block_given?
     block_given? ? self : enum_for(__method__)
   end
 
@@ -31,7 +31,7 @@ module Enumerable
         return arg1 if (question[0] === item) == arg2
       elsif !block[0].nil?
         return arg1 if block[0].call(item) == arg2
-      elsif ([nil, false].include? item  ) == !arg2
+      elsif ([nil, false].include? item) == !arg2
         return arg1
       end
     end
@@ -100,16 +100,14 @@ proc = proc { |n| n * 2 }
 p %w[cat dog].my_map(proc)
 
 # arr = [ true ]
+# arr2 = %w[ant bear cat]
 # words = ["none","any","all"]
 # var = "none"
-# 
 # words.each do |var|
-# 
-# p eval("%w[ant bear cat].my_#{var}? { |word| word.length >= 3 } == %w[ant bear cat].#{var}? { |word| word.length >= 3 }")
-# p eval("%w[ant bear cat].my_#{var}? { |word| word.length >= 4 } == %w[ant bear cat].#{var}? { |word| word.length >= 4 }")
-# p eval("%w[ant bear cat].my_#{var}?(/d/)                        == %w[ant bear cat].#{var}?(/d/)                       ")
-# p eval("arr.my_#{var}?(Integer)                     == arr.#{var}?(Integer)                    ")
-# p eval("arr.my_#{var}?                              == arr.#{var}?                             ")
-# p eval("[].my_#{var}?                                           == [].#{var}?                                          ")
-# 
+# p eval("arr2.my_#{var}? { |word| word.length >= 3 } == arr2.#{var}? { |word| word.length >= 3 }")
+# p eval("arr2.my_#{var}? { |word| word.length >= 4 } == arr2.#{var}? { |word| word.length >= 4 }")
+# p eval("arr2.my_#{var}?(/d/)== arr2.#{var}?(/d/)")
+# p eval("arr.my_#{var}?(Integer)== arr.#{var}?(Integer)")
+# p eval("arr.my_#{var}? == arr.#{var}?")
+# p eval("[].my_#{var}? == [].#{var}?")
 # end
